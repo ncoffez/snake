e5f22c87f7d1cb88c197e59c8bcb163bb168ce38
ncoffez <ncoffez@gmail.com>
2023-01-29 14:00:53 +0100

    configured permissions

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['hgroup'].includes(tag),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => ["hgroup"].includes(tag),
+          },
         },
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }),
+    ],
+    build: {
+      target: "esnext",
     },
-  },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
